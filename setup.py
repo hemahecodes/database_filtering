2e71600ddc996d959c1650ba693b8a84234458f8
Helena Martín Hernández <58825933+hemahecodes@users.noreply.github.com>
2021-11-02 12:04:53 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'filtering',
+  packages = ['filtering'],
+  version = '0.1',
+  license='MIT',
+  description = 'Filter database with r_group specification.',
+  author = 'Helena Martín',
+  author_email = 'helena.martin@nostrumbiodiscovery.com',
+  url = 'https://github.com/hemahecodes/filtering',
+  download_url = 'https://github.com/hemahecodes/filtering/archive/v_01.tar.gz',
+  keywords = ['filtering', 'database', 'rdkit'],   #
+  install_requires=[
+          'validators',
+          'beautifulsoup4',
+          'rdkit'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
