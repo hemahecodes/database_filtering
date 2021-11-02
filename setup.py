b8cebbce5a4d4054c33cfcd048c37829fd85a9c1
hemahecodes <helena.martin@nostrumbiodiscovery.com>
2021-11-02 12:43:28 +0100

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'filtering',
-  packages = ['filtering'],
+  name = 'database_filtering',
+  packages = ['database_filtering'],
   version = '0.1',
   license='MIT',
   description = 'Filter database with r_group specification.',
   author = 'Helena Martín',
   author_email = 'helena.martin@nostrumbiodiscovery.com',
-  url = 'https://github.com/hemahecodes/filtering',
-  download_url = 'https://github.com/hemahecodes/filtering/archive/refs/tags/0.1.zip',
+  url = 'https://github.com/hemahecodes/database_filtering',
+  download_url = 'https://github.com/hemahecodes/database_filtering/archive/refs/tags/0.1.zip',
   keywords = ['filtering', 'database', 'rdkit'],   #
   install_requires=[
           'validators',
